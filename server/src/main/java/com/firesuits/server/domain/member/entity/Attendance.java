eef43435a0227f9398e60d86c3ecd9e42c835f97
Gonue <6017891@naver.com>
2023-07-10 21:27:49 +0900

    refactor : member entity

--- server/src/main/java/com/firesuits/server/domain/member/entity/Attendance.java
+++ server/src/main/java/com/firesuits/server/domain/member/entity/Attendance.java
@@ -10,15 +10,19 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Getter
 @Setter
+@Table(name = "attendance")
 @Entity
 public class Attendance {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "attendance_id", updatable = false)
     private Long attendanceId;
 
     @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
+
+    @Column(name = "check_date")
     private LocalDate  checkDate;
 }
--- server/src/main/java/com/firesuits/server/domain/member/entity/PasswordReset.java
+++ server/src/main/java/com/firesuits/server/domain/member/entity/PasswordReset.java
@@ -8,16 +8,21 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Table(name = "password_reset")
 @Entity
 public class PasswordReset {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "password_reset_id", updatable = false)
     private Long passwordResetId;
 
     @OneToOne(targetEntity = Member.class, fetch = FetchType.EAGER)
     @JoinColumn(nullable = false, name = "memer_id")
     private Member member;
 
+    @Column(name = "token")
     private String token;
+
+    @Column(name = "expiration")
     private LocalDateTime expiration;
 }
